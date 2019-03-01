68105a8be65af8b485bb85f87774816a233e767f
Mark Struzinski <ski081@gmail.com>
2019-02-28 19:26:36 -0500

    Initial Commit

---
+++ DevSlopesSlack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DevSlopesSlack
+//
+//  Created by Mark Struzinski on 2/28/19.
+//  Copyright © 2019 BobStruzSoftware. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
